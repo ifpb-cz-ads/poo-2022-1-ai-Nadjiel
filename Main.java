3e44f365d0b92de555da2665491348fe78b85fd9
Daniel de O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2022-05-19 13:48:06 -0300

    questao: semana 4

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      
         int FPS = 60;
         String jogo = "Xadrez";
         int larguraTela = 500;
@@ -11,11 +15,32 @@ public class Main {
         System.out.print("em uma tela de " + larguraTela + " por " + alturaTela + " ");
         System.out.print("no modo ");
         if(paisagem) {
-            System.out.print("paisagem.");
+            System.out.print("paisagem.\n");
         }
         else {
-            System.out.print("retrato.");
+            System.out.print("retrato.\n");
         }
+
+        System.out.print("Qual peca voce gostaria de mexer? (t, b, c, R, D, ou p) ");
+        char peca = scanner.nextLine().charAt(0);
+        
+        switch(peca) {
+            case 't': System.out.println("Movida a torre.");
+                    break;
+            case 'b': System.out.println("Movido o bispo.");
+                    break;
+            case 'c': System.out.println("Movido o cavalo.");
+                    break;
+            case 'R': System.out.println("Movido o rei.");
+                    break;
+            case 'D': System.out.println("Movida a dama.");
+                    break;
+            case 'p': System.out.println("Movido o peao.");
+                    break;
+            default: System.out.println("Peca inserida invalida.");
+        }
+
+        scanner.close();
     }
 
 }
