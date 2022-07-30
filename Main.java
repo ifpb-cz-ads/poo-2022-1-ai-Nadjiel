ae3313699dcc505fe34758615fc0ad20a54f17bc
Daniel de O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2022-07-30 12:26:21 -0300

    adicionado metodo main

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics2D;
+
+import imagine.game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Game() {
+
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onUpdate() {
+                
+            }
+
+            @Override
+            public void onDraw(Graphics2D g2) {
+                
+            }
+
+        };
+    }
+
+}
